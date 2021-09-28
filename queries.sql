a592dbb6df0d78b2c785d1ec964df09975c1f4a0
Caleb Anderson <buyananderson@gmail.com>
2021-09-28 16:27:01 +0200

    Select data from the table 2

--- queries.sql
+++ queries.sql
@@ -1,17 +1,17 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * FROM animals WHERE name LIKE '%pus';
+SELECT * FROM animals WHERE name LIKE '%mon';
 
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 
-SELECT name FROM animals WHERE neutered=true AND escape_attempts<2;
+SELECT name FROM animals WHERE neutered=true AND escape_attempts<3;
 
-SELECT date_of_birth FROM animals WHERE name='Saggapus' OR name='rex';
+SELECT date_of_birth FROM animals WHERE name='Agumon' OR name='Pikachu';
 
-SELECT name, escape_attempts FROM animals WHERE weight_kg>8;
+SELECT name, escape_attempts FROM animals WHERE weight_kg>10.5;
 
 SELECT * FROM animals WHERE neutered=true;
 
-SELECT * FROM animals WHERE NOT name='Milu';
+SELECT * FROM animals WHERE NOT name='Gabumon';
 
-SELECT * FROM animals WHERE weight_kg BETWEEN 10 AND 20;
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
