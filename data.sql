c29cb1e36d3816cfb884afdd937edaff212aa643
Caleb Anderson <buyananderson@gmail.com>
2021-09-28 16:26:04 +0200

    Insert data in the database 2

--- data.sql
+++ data.sql
@@ -1,6 +1,7 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('REX', '03-02-2020', 0, TRUE, 20);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Saggapus', '15-11-2020', 12, TRUE, 8);
-INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Pikachu', '7-01-2021', 10, FALSE, 19.04);
-INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Milu', '12-05-2017', 7, TRUE, 14.10);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', '03-02-2020', 0, TRUE, 10.23);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', '15-11-2020', 2, TRUE, 8);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Pikachu', '7-01-2021', 1, FALSE, 15.04);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Devimon', '12-05-2017', 5, TRUE, 11);
