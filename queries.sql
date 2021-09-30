72ceb46f1307f05bb0c11af1b27a9b8e65d7dd36
Caleb Anderson <buyananderson@gmail.com>
2021-09-30 08:19:50 +0200

    --  Who owns the most animals?

--- queries.sql
+++ queries.sql
@@ -89,3 +89,6 @@ SELECT animals.name, animals.escape_attempts FROM animals INNER JOIN owners ON o
   WHERE owners.full_name = 'Dean Winchester'
   AND animals.escape_attempts = 0;
 
+--  Who owns the most animals?
+SELECT owners.full_name, COUNT(animals.owner_id) FROM animals FULL OUTER JOIN owners ON animals.owner_id = owners.id
+    GROUP BY owners.id; 
