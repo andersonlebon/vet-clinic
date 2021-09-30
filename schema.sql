3f249b1b02598ef47db5b4578c16f1e6d58f70bc
Caleb Anderson <buyananderson@gmail.com>
2021-09-30 07:18:51 +0200

    Add column owner_id which is a foreign key referencing the owners table 2

--- schema.sql
+++ schema.sql
@@ -35,14 +35,14 @@ ALTER TABLE animals ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY;
 -- Remove the spacies column in the animals table
 ALTER TABLE animals DROP species;
 
-ALTER TABLE animals ADD owner_id INT; 
-ALTER TABLE animals ADD species_id INT;
 
 
 -- Add column species_id which is a foreign key referencing species table
+ALTER TABLE animals ADD species_id INT;
 ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species(id); 
 
 -- Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE animals ADD owner_id INT; 
 ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
 
 
