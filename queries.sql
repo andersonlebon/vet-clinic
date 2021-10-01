1b6f307221471e9f4464f672d7a4df40e059d47b
Caleb Anderson <buyananderson@gmail.com>
2021-10-01 11:59:15 +0200

    Query Who was Maisy Smith's first visit

--- queries.sql
+++ queries.sql
@@ -124,3 +124,11 @@ SELECT animals.name, COUNT(visits.animals_id) AS visit_count FROM visits
     INNER JOIN animals ON animals.id = visits.animals_id
     GROUP BY animals.name, visits.animals_id
     ORDER BY visit_count DESC LIMIT 1;
+
+--  Who was Maisy Smith's first visit?
+SELECT animals.name, visits.date_of_visit AS first_visit FROM visits
+    INNER JOIN vets ON vets.id = visits.vets_id
+    INNER JOIN animals ON animals.id = visits.animals_id
+    WHERE vets.name = 'Maisy Smith'
+    GROUP BY animals.name, visits.date_of_visit
+    ORDER BY first_visit LIMIT 1;
